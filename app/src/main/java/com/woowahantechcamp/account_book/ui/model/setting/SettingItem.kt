c6573ef6823c7141584bba5a9912af1241eba80b
Jihye Joo <oreocube13@gmail.com>
2022-07-27 19:53:43 +0900

    [UI] #6 - 설정화면 기본 레이아웃 구성
    - 제목은 Header, 리스트 아이템은 Body, 추가 버튼은 Footer 로 구성
    - sealed class 를 사용해 리스트 아이템 타입을 묶음
    - 리스트 아이템 타입이 CategoryItem 인 경우 태그 표시

---
+++ app/src/main/java/com/woowahantechcamp/account_book/ui/model/setting/SettingItem.kt
@@ -0,0 +1,16 @@
+package com.woowahantechcamp.account_book.ui.model.setting
+
+import androidx.compose.ui.graphics.Color
+
+sealed class SettingItem(
+    open val title: String
+) {
+    data class TextItem(
+        override val title: String
+    ) : SettingItem(title)
+
+    data class CategoryItem(
+        override val title: String,
+        val color: Color
+    ) : SettingItem(title)
+}
