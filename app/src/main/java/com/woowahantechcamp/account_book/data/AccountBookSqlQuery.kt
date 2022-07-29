86b60a58c386df4477bd32a741cefe5228ff6f23
Jihye Joo <oreocube13@gmail.com>
2022-07-29 22:44:30 +0900

    [CHORE] #12 - 쿼리문 한 곳에서 관리하도록 이동

--- app/src/main/java/com/woowahantechcamp/account_book/data/AccountBookSqlQuery.kt
+++ app/src/main/java/com/woowahantechcamp/account_book/data/AccountBookSqlQuery.kt
@@ -5,7 +5,35 @@ import com.woowahantechcamp.account_book.data.repository.CategoryEntry
 import com.woowahantechcamp.account_book.data.repository.HistoryEntry
 import com.woowahantechcamp.account_book.data.repository.PaymentEntry
 
-const val SELECT_ALL_HISTORIES =
+const val SQL_CREATE_HISTORY_TABLE =
+    "CREATE TABLE ${HistoryEntry.TABLE_NAME} (" +
+            "${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "${HistoryEntry.COLUMN_NAME_TYPE} INTEGER NOT NULL, " +
+            "${HistoryEntry.COLUMN_NAME_DATE} DATE NOT NULL, " +
+            "${HistoryEntry.COLUMN_NAME_AMOUNT} INTEGER NOT NULL, " +
+            "${HistoryEntry.COLUMN_NAME_PAYMENT_ID} INTEGER NOT NULL, " +
+            "${HistoryEntry.COLUMN_NAME_CATEGORY_ID} INTEGER NOT NULL, " +
+            "${HistoryEntry.COLUMN_NAME_CONTENT} TEXT, " +
+            "FOREIGN KEY (${HistoryEntry.COLUMN_NAME_PAYMENT_ID}) " +
+            "REFERENCES ${PaymentEntry.TABLE_NAME} (${BaseColumns._ID}), " +
+            "FOREIGN KEY (${HistoryEntry.COLUMN_NAME_CATEGORY_ID}) " +
+            "REFERENCES ${CategoryEntry.TABLE_NAME} (${BaseColumns._ID}))"
+
+const val SQL_CREATE_PAYMENT_TABLE =
+    "CREATE TABLE ${PaymentEntry.TABLE_NAME} (" +
+            "${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "${PaymentEntry.COLUMN_NAME_TITLE} TEXT NOT NULL)"
+
+const val SQL_CREATE_CATEGORY_TABLE =
+    "CREATE TABLE ${CategoryEntry.TABLE_NAME} (" +
+            "${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "${CategoryEntry.COLUMN_NAME_TYPE} INTEGER NOT NULL, " +
+            "${CategoryEntry.COLUMN_NAME_TITLE} TEXT NOT NULL, " +
+            "${CategoryEntry.COLUMN_NAME_COLOR} TEXT NOT NULL)"
+
+const val SQL_SET_PRAGMA = "PRAGMA foreign_keys = ON"
+
+const val SQL_SELECT_ALL_HISTORIES =
     "SELECT ${HistoryEntry.TABLE_NAME}.${BaseColumns._ID}, " +
             "${HistoryEntry.TABLE_NAME}.${HistoryEntry.COLUMN_NAME_TYPE}, " +
             "${HistoryEntry.TABLE_NAME}.${HistoryEntry.COLUMN_NAME_DATE}, " +
