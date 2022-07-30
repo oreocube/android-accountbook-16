e3a98f13a489541f7431567477b89ead9a71b79d
Jihye Joo <oreocube13@gmail.com>
2022-07-30 15:03:27 +0900

    [UI] #21 - 필터 컴포넌트 구현
    - 필터 컴포넌트 구현
    - 수입/지출 모두 선택/해제 가능하므로 각 상태를 파라미터로 받음
    - 클릭 이벤트 발생 시 상태 값 변경
    - 천원 단위로 보여주는 확장 함수 추가

---
+++ app/src/main/java/com/woowahantechcamp/account_book/ui/component/FilterButton.kt
@@ -0,0 +1,94 @@
+package com.woowahantechcamp.account_book.ui.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.woowahantechcamp.account_book.R
+import com.woowahantechcamp.account_book.ui.model.setting.Type
+import com.woowahantechcamp.account_book.ui.theme.LightPurple
+import com.woowahantechcamp.account_book.ui.theme.White
+import com.woowahantechcamp.account_book.util.toCurrency
+
+@Composable
+fun FilterButton(
+    incomeChecked: MutableState<Boolean>,
+    expenseChecked: MutableState<Boolean>,
+    modifier: Modifier
+) {
+    Surface(
+        shape = RoundedCornerShape(10.dp),
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(10.dp))
+        ) {
+            Type.values().forEach {
+                FilterItem(
+                    type = it,
+                    amount = 2345600, // TODO
+                    selected = if (it == Type.INCOME) incomeChecked else expenseChecked,
+                    modifier = Modifier
+                        .weight(1f)
+                        .clickable {
+                            if (it == Type.INCOME) incomeChecked.value = !incomeChecked.value
+                            else expenseChecked.value = !expenseChecked.value
+                        })
+            }
+        }
+    }
+}
+
+@Composable
+fun FilterItem(
+    type: Type,
+    amount: Int,
+    selected: MutableState<Boolean>,
+    modifier: Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = if (selected.value) MaterialTheme.colors.primary else LightPurple)
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Surface(
+            modifier = Modifier
+                .size(12.dp)
+                .clip(RoundedCornerShape(2.dp)),
+            border = BorderStroke(1.dp, White),
+            color = if (selected.value) White else Color.Transparent
+        ) {
+            if (selected.value) {
+                Image(
+                    painter = painterResource(R.drawable.ic_check),
+                    contentDescription = type.title,
+                )
+            }
+        }
+        Spacer(modifier = Modifier.width(5.dp))
+        Text(
+            text = "${type.title} ${amount.toCurrency()}",
+            fontSize = 12.sp,
+            color = White
+        )
+    }
+}
