f6f3bbf068bf17f38a9e00a7309ba0a436fc5a42
Jihye Joo <oreocube13@gmail.com>
2022-07-30 22:04:50 +0900

    [UI] #19 - 팔레트 컴포넌트 구현

---
+++ app/src/main/java/com/woowahantechcamp/account_book/ui/component/ColorPalette.kt
@@ -0,0 +1,64 @@
+package com.woowahantechcamp.account_book.ui.component
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.woowahantechcamp.account_book.ui.theme.*
+import com.woowahantechcamp.account_book.util.expenseColorList
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ColorPalette(
+    selectedIndex: Int = 0,
+    colorList: List<Color>,
+    onSelectChanged: (Int) -> Unit
+) {
+    LazyVerticalGrid(
+        modifier = Modifier.fillMaxWidth(),
+        cells = GridCells.Fixed(10),
+        verticalArrangement = Arrangement.Center
+    ) {
+        colorList.forEachIndexed { index, color ->
+            item {
+                Box(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .size(24.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(if (index == selectedIndex) 24.dp else 16.dp)
+                            .align(Alignment.Center)
+                            .background(color = color)
+                            .clickable { onSelectChanged(index) }) {
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PalettePreview() {
+    AccountbookTheme {
+
+        val selectedColorIndex = rememberSaveable { mutableStateOf(0) }
+        ColorPalette(
+            selectedIndex = selectedColorIndex.value,
+            colorList = expenseColorList,
+            onSelectChanged = { selectedColorIndex.value = it }
+        )
+    }
+}
---
+++ app/src/main/java/com/woowahantechcamp/account_book/util/ColorList.kt
@@ -0,0 +1,15 @@
+package com.woowahantechcamp.account_book.util
+
+import com.woowahantechcamp.account_book.ui.theme.*
+
+val incomeColorList = listOf(
+    Olive1, Olive2, Olive3, Olive4, Olive5,
+    Yellow1, Yellow2, Yellow3, Yellow4, Yellow5,
+)
+
+val expenseColorList = listOf(
+    Blue1, Blue2, Blue3, Blue4, Blue5,
+    Green1, Green2, Green3, Green4, Green5,
+    Purple1, Purple2, Purple3, Purple4, Purple5,
+    Pink1, Pink2, Pink3, Pink4, Pink5,
+)
