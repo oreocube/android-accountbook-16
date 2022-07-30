ce7238a9eb88a6d57f119ecc2f4b5a6cfcfa5b93
Jihye Joo <oreocube13@gmail.com>
2022-07-30 15:28:21 +0900

    [UI] #18 - 토글 버튼 컴포넌트 구현

---
+++ app/src/main/java/com/woowahantechcamp/account_book/ui/component/SwitchButton.kt
@@ -0,0 +1,85 @@
+package com.woowahantechcamp.account_book.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.woowahantechcamp.account_book.ui.model.setting.Type
+import com.woowahantechcamp.account_book.ui.theme.AccountbookTheme
+import com.woowahantechcamp.account_book.ui.theme.LightPurple
+import com.woowahantechcamp.account_book.ui.theme.White
+
+@Composable
+fun SwitchButton(
+    currentType: MutableState<Type>,
+    modifier: Modifier
+) {
+    Surface(
+        shape = RoundedCornerShape(10.dp),
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(10.dp))
+        ) {
+            Type.values().forEach { type ->
+                SwitchButtonItem(
+                    type = type,
+                    selected = type == currentType.value,
+                    onClick = { currentType.value = it },
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun SwitchButtonItem(
+    type: Type,
+    selected: Boolean,
+    onClick: (Type) -> Unit,
+    modifier: Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable { onClick(type) }
+            .background(color = if (selected) MaterialTheme.colors.primary else LightPurple)
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = type.title,
+            fontSize = 12.sp,
+            color = White
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSwitchButton() {
+    AccountbookTheme {
+        val currentType = remember { mutableStateOf(Type.INCOME) }
+        SwitchButton(currentType = currentType, modifier = Modifier)
+    }
+}
