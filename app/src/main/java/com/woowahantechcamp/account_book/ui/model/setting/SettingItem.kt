5b92d336ada76258dfd173fbcd36f86a28e3a8c4
Jihye Joo <oreocube13@gmail.com>
2022-07-31 19:22:34 +0900

    [ADD] #26 - 설정화면 클릭 이벤트 처리

--- app/src/main/java/com/woowahantechcamp/account_book/ui/model/setting/SettingItem.kt
+++ app/src/main/java/com/woowahantechcamp/account_book/ui/model/setting/SettingItem.kt
@@ -3,15 +3,18 @@ package com.woowahantechcamp.account_book.ui.model.setting
 import androidx.compose.ui.graphics.Color
 
 sealed class SettingItem(
+    open val id: Int,
     open val title: String
 ) {
     data class TextItem(
+        override val id: Int,
         override val title: String
-    ) : SettingItem(title)
+    ) : SettingItem(id, title)
 
     data class CategoryItem(
+        override val id: Int,
         override val title: String,
         val type: Type,
         val color: Color
-    ) : SettingItem(title)
+    ) : SettingItem(id, title)
 }
