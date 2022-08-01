417f9ea0f3c5d3b4690c1b547eaadaf5e49ba1ff
Jihye Joo <oreocube13@gmail.com>
2022-08-01 10:26:41 +0900

    [FIX] #28 - 내역 조회 쿼리 문법 오류 수정
    - 처음에는 JOIN 관련 문제인 줄 알았으나 띄어쓰기 또는 날짜형식 관련 문제였던 것으로 보인다

--- app/src/main/java/com/woowahantechcamp/account_book/data/AccountBookSqlQuery.kt
+++ app/src/main/java/com/woowahantechcamp/account_book/data/AccountBookSqlQuery.kt
@@ -44,7 +44,7 @@ const val SQL_SELECT_ALL_HISTORIES =
             "${PaymentEntry.TABLE_NAME}.${PaymentEntry.COLUMN_NAME_TITLE}, " +
             "${CategoryEntry.TABLE_NAME}.${CategoryEntry.COLUMN_NAME_TITLE}, " +
             "${CategoryEntry.TABLE_NAME}.${CategoryEntry.COLUMN_NAME_COLOR} " +
-            "FROM ${HistoryEntry.TABLE_NAME}, ${PaymentEntry.TABLE_NAME}, ${CategoryEntry.TABLE_NAME}" +
+            "FROM ${HistoryEntry.TABLE_NAME}, ${PaymentEntry.TABLE_NAME}, ${CategoryEntry.TABLE_NAME} " +
             "WHERE ${HistoryEntry.COLUMN_NAME_PAYMENT_ID} = ${PaymentEntry.TABLE_NAME}.${BaseColumns._ID} " +
             "AND ${HistoryEntry.COLUMN_NAME_CATEGORY_ID} = ${CategoryEntry.TABLE_NAME}.${BaseColumns._ID} " +
-            "AND ${HistoryEntry.COLUMN_NAME_DATE} BETWEEN ? AND ?"
+            "AND ${HistoryEntry.COLUMN_NAME_DATE} BETWEEN date(?) AND date(?)"
